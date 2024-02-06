b3b3a3708700b0c574c66545532069d5e79e2dea
Matusal3m <matusalemdedeus@gmail.com>
2024-02-06 17:01:31 -0300

    Initial commit

---
+++ gêese.js
@@ -0,0 +1,15 @@
+// const cannabis = document.getElementById("tecla_maconha")
+
+// const geme = document.getElementById("tecla_geme")
+
+// const romantic = document.getElementById("tecla_romantica")
+
+// const kawasaki = document.getElementById("tecla_kawasaki")
+
+// const laele = document.getElementById("tecla_laele")
+
+// const cavalo = document.getElementById("tecla_cavalo")
+
+const teclas = document.querySelectorAll('container')
+
+console.log(teclas)
