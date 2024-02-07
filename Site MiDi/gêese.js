f33b057a1970563811ccb4b2b799e207716f9c8a
Kayo Lucas Lima <124395376+KLL7@users.noreply.github.com>
2024-02-06 21:12:50 -0300

    Add files via upload

---
+++ Site MiDi/gêese.js
@@ -0,0 +1,35 @@
+function tocacensura() {
+    document.querySelector('#weed').play();
+}
+
+function tocageme() {
+    document.querySelector('#som_geme').play();
+}
+
+function tocaromantic() {
+    document.querySelector('#romantic').play();
+}
+
+function tocakawasaki() {
+    document.querySelector('#som_kawasaki').play();
+}
+
+function tocalaele() {
+    document.querySelector('#som_laele').play();
+}
+
+function tocacavalo() {
+    document.querySelector('#som_cavalo').play();
+}
+
+function tocafilho() {
+    document.querySelector('#som_filho').play();
+}
+
+function tocauepa() {
+    document.querySelector('#som_uepa').play();
+}
+
+function tocamundial() {
+    document.querySelector('#som_mundial').play();
+}
