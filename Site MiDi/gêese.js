6aced9886e5cdbed038d684b2fdacce4064aaa8d
Matusal3m <matusalemdedeus@gmail.com>
2024-02-07 00:28:48 -0300

    televisão e videos

--- Site MiDi/gêese.js
+++ Site MiDi/gêese.js
@@ -1,33 +1,82 @@
 function tocacensura() {
     document.querySelector('#weed').play();
+    let video = document.getElementById('maconha');
+    video.style.display = 'block';
+    video.play();
+    video.addEventListener('ended', function() {
+        video.style.display = 'none';
+    });
+
 }
 
 function tocageme() {
     document.querySelector('#som_geme').play();
+    let video = document.getElementById('geme');
+    video.style.display = 'block';
+    video.play();
+    video.addEventListener('ended', function() {
+        video.style.display = 'none';
+    });
 }
 
 function tocaromantic() {
     document.querySelector('#romantic').play();
+    let video = document.getElementById('romantico');
+    video.style.display = 'block';
+    video.play();
+    video.addEventListener('ended', function() {
+        video.style.display = 'none';
+    });
 }
 
 function tocakawasaki() {
     document.querySelector('#som_kawasaki').play();
+    let video = document.getElementById('kawazaki');
+    video.style.display = 'block';
+    video.play();
+    video.addEventListener('ended', function() {
+        video.style.display = 'none';
+    });
 }
 
 function tocalaele() {
     document.querySelector('#som_laele').play();
+    let video = document.getElementById('video_laele');
+    video.style.display = 'block';
+    video.play();
+    video.addEventListener('ended', function() {
+        video.style.display = 'none';
+    });
 }
 
 function tocacavalo() {
     document.querySelector('#som_cavalo').play();
+    let video = document.getElementById('cavalo');
+    video.style.display = 'block';
+    video.play();
+    video.addEventListener('ended', function() {
+        video.style.display = 'none';
+    });
 }
 
 function tocafilho() {
     document.querySelector('#som_filho').play();
+    let video = document.getElementById('queissomeufi');
+    video.style.display = 'block';
+    video.play();
+    video.addEventListener('ended', function() {
+        video.style.display = 'none';
+    });
 }
 
 function tocauepa() {
     document.querySelector('#som_uepa').play();
+    let video = document.getElementById('uepa');
+    video.style.display = 'block';
+    video.play();
+    video.addEventListener('ended', function() {
+        video.style.display = 'none';
+    });
 }
 
 function tocamundial() {
